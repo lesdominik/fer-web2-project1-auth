9239db396270c841bbc1b23b3816b2e3ab422910
Dominik Leš <dominikles@Dominiks-MacBook-Air.local>
2023-10-28 20:55:07 +0200

    Create functional server

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      CompetitionTracker
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+app.listen(3000, () => {
+  console.log("Express is running on port 3000");
+});
